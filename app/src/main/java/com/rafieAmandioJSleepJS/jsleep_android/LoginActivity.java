32a7abb121fc531f306a798b61d852f91ed2eba9
IdleGoat <rafie.amandio@ui.ac.id>
2022-11-15 17:42:06 +0700

    CS Modul 8

---
+++ app/src/main/java/com/rafieAmandioJSleepJS/jsleep_android/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.rafieAmandioJSleepJS.jsleep_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.rafieAmandioJSleepJS.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(move);
+            }
+        });
+        Button loginbutton = findViewById(R.id.loginbutton);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(move);
+            }
+        });
+
+
+    }
+}
